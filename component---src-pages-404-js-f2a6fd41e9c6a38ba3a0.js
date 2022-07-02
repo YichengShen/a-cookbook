c8b288b3639fa8dddcf3feee2ebc07523174e4a3
YichengShen <jsszsyc@hotmail.com>
2022-07-02 14:42:37 +0800

    Updates

---
+++ component---src-pages-404-js-f2a6fd41e9c6a38ba3a0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{429:function(t,e,a){a.r(e);a(7294);var s=a(953),n=a(262),o=a(3431);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return(0,o.tZ)(s.Z,{location:a,title:u},(0,o.tZ)(n.Z,{title:"404: Not Found"}),(0,o.tZ)("h1",null,"404: Not Found"),(0,o.tZ)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f2a6fd41e9c6a38ba3a0.js.map
